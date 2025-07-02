9b529910efddb10ed6c30bc563c4cd3b17feb6ed
ZenZxnZen <Rizeenroe00@gmail.com>
2025-07-02 16:16:12 -0400

    initial commit, zenservers frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { log } from 'console';
+import './App.css'
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [serverInfo, setServerInfo] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+  fetch('http://localhost:3001/api/mc-status')
+    .then((res) => res.json())
+    .then(data => {
+      console.log(data); 
+      setServerInfo(data);  
+    })
+    .catch((err) => setError(err.message));
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="app-container">
+      <h1>Zen Servers</h1>
+      <div id='minecraft'>
+        <img src="https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2020-06-image14-3.png" alt="" />
+        <p>{serverInfo?.motd?.clean ?? "Loading MOTD..."}</p>
+        <p>Host: {serverInfo?.srvRecord.host ?? "Loading..."}</p>
+        <p>Port: {serverInfo?.srvRecord.port ?? "Loading..."}</p>
+        <p>Version: {serverInfo?.version?.name ?? "Loading..."}</p>
+        <p>Online: {serverInfo?.players?.online ?? 0} / {serverInfo?.players?.max ?? 0}</p>
+        <a href="/downloads/minecraft/zen server 1.0.0.mrpack" download>
+          Dwonload Modrinth Mod Pack
         </a>
-      </header>
+      </div>
     </div>
   );
-}
 
+}
 export default App;
